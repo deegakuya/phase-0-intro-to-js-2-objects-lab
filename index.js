8e2a75560e6b5ee82a2208de7482854f13797270
Diana Gakuya <deegakuya@gmail.com>
2023-09-23 17:16:58 +0300

    Commit 3

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Kate',
+    streetAddress: '827 MonicaLewinsky',
+  };
+  
+  // Function to update an employee Object with a new key-value pair
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+  // Function to destructively update an employee Object with a new key-value pair
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key-value pair from an employee Object non-destructively
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete a key-value pair from an employee Object destructively
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
